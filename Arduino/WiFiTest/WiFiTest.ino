f71460b932635e65d6fe82c46b85e395a36ab8f3
ClemF <ClemF@github.com>
2018-03-31 01:52:21 -0400

    slowworking program

--- Arduino/WiFiTest/WiFiTest.ino
+++ Arduino/WiFiTest/WiFiTest.ino
@@ -1,105 +1,86 @@
-#include <WiFi.h>
-#include <WiFiUdp.h>
-#include <ArduinoOSC.h>
+//172.20.80.181
 
-WiFiUDP udp;
-ArduinoOSC<WiFiUDP> osc;
+#include <SoftwareSerial.h>
+#include <string.h>
 
-const char* ssid = "Private-Notman";
-const char* pwd = "3S6MpezhnHd";
-//const IPAddress ip(192, 168, 1, 201);
-//const IPAddress gateway(192, 168, 1, 1);
-//const IPAddress subnet(255, 255, 255, 0);
-const char* host = "172.20.80.27";
-const int recv_port = 52193;
-const int send_port = 12000;
-
+#define TIMEOUT 5000 // mS
+#define LED 5
+SoftwareSerial mySerial(0, 1); // RX, TX
+int ind1, ind2;
+ 
 void setup()
 {
-    Serial.begin(115200);
-//    WiFi.disconnect(true);
-//    WiFi.onEvent(onWiFiEvent);
-    WiFi.begin(ssid, pwd);
-//    WiFi.config(ip, gateway, subnet);
-
-    // check for the presence of the shield:
-    if (WiFi.status() == WL_NO_SHIELD) {
-      Serial.println("WiFi shield not present");
-      // don't continue:
-      while (true);
-    }
-
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        delay(500);
-        Serial.println(WiFi.status());
-    }
-
-//    osc.begin(udp, recv_port);
-//    osc.addCallback("/ard/aaa", &callback);
-//    osc.addCallback("/ard", &callback);
+ pinMode(0, INPUT);
+ pinMode(1, OUTPUT);
+ 
+ Serial.begin(9600);
+ mySerial.begin(115200);
+ Serial.println("INITIATION");
+ //SendCommand("AT+RST", "Ready");
+ //delay(5000);
+ //SendCommand("AT+CWMODE=1","OK");
+ //SendCommand("AT+CIFSR", "OK");
+ //SendCommand("AT+CIPMUX=1","OK");
+ //SendCommand("AT+CIPSERVER=1,80","OK");
 }
-
-void loop()
-{
-//    osc.parse();
-
-      if (WiFi.status() == WL_CONNECTED) {
-    Serial.println();
-    Serial.println("connected!");
-   
+ 
+void loop(){
+ String IncomingString="";
+ String output = "";
+ int data;
+ ind1 = ind2 = -1;
+ //StringReady= true;
+ if (mySerial.available()){
+   IncomingString=mySerial.readString();
+   data = IncomingString.toInt();
+   if (data > -10 && data < 10){
+    output = IncomingString.substring(11, 15);
+    Serial.println("DATA : " + output);
+    
+   }
+    /*
+      for (int i = 0; i<IncomingString.length(); i++){
+        if (IncomingString.charAt(i) == ':' && ind1 == -1){
+          ind1 = i;
+        }
+        else if(IncomingString.charAt(i) == ':' && ind2 == -1){
+          ind2 = i;
+        }
+      }
+      
+      if(ind1 != -1 && ind2!=-1){
+       Serial.println("DATA : " + IncomingString.substring(ind1, ind2));
+      }
+     }
+     */
+ }
+ if (Serial.available()){
+    SendCommand(Serial.readString(), "ok");
+ }
+ 
+ delay(10);
+}
+ 
+boolean SendCommand(String cmd, String ack){
+  mySerial.println(cmd); // Send "AT+" command to module
+  if (!echoFind(ack)) // timed out waiting for ack string
+    return true; // ack blank or ack found
+}
+ 
+boolean echoFind(String keyword){
+ byte current_char = 0;
+ byte keyword_length = keyword.length();
+ long deadline = millis() + TIMEOUT;
+ while(millis() < deadline){
+  if (mySerial.available()){
+    char ch = mySerial.read();
+    Serial.write(ch);
+    if (ch == keyword[current_char])
+      if (++current_char == keyword_length){
+       Serial.println();
+       return true;
+    }
+   }
   }
-  Serial.println(WiFi.status());
+ return false; // Timed out
 }
-//
-//void callback(OSCMessage& m)
-//{
-//    //create new osc message
-//    OSCMessage msg;
-//    //set destination ip address & port no
-//    msg.beginMessage(host, send_port);
-//    //set argument
-//    msg.setOSCAddress(m.getOSCAddress());
-//    msg.addArgInt32(m.getArgAsInt32(0));
-//    msg.addArgFloat(m.getArgAsFloat(1));
-//    msg.addArgString(m.getArgAsString(2));
-//    //send osc message
-//    osc.send(msg);
-//}
-
-//void onWiFiEvent(WiFiEvent_t event)
-//{
-//    switch (event)
-//    {
-//        case SYSTEM_EVENT_STA_START:
-//        {
-//            Serial.println("STA Started");
-//            break;
-//        }
-//        case SYSTEM_EVENT_STA_CONNECTED:
-//        {
-//            Serial.println("STA Connected");
-//            break;
-//        }
-//        case SYSTEM_EVENT_STA_GOT_IP:
-//        {
-//            Serial.print("STA IPv4: ");
-//            Serial.println(WiFi.localIP());
-//            break;
-//        }
-//        case SYSTEM_EVENT_STA_DISCONNECTED:
-//        {
-//            Serial.println("STA Lost Connection");
-//            break;
-//        }
-//        case SYSTEM_EVENT_STA_STOP:
-//        {
-//            Serial.println("STA Stopped");
-//            break;
-//        }
-//        default:
-//        {
-//            break;
-//        }
-//    }
-//}
